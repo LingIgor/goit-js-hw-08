721e99091de4da214c44ac3a28a2d85aaf7dc9b9
LingIgor <lingigor1990@gmail.com>
2023-03-06 11:33:23 +0200

    third hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea')
+}
+ const STORAGE_KEY = 'feedback-form-state'
+console.log(refs.form)
+console.log(refs.textarea)
 
+refs.form.addEventListener('submit', onFormSubmit)
+refs.textarea.addEventListener('input', onTextAreaInput)
+
+function onFormSubmit (e) {
+e.preventDefault();
+console.log('otpravlyaem formu');
+e.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY)
+}
+
+
+function onTextAreaInput (e) {
+const massage = e.currentTarget.value;
+localStorage.setItem(STORAGE_KEY, massage);
+
+}
