ff6f1d0e3d63d2a4a127afbfd99ba057a97a48e6
Ling Igor <lingigor1990@gmail.com>
2023-03-07 21:29:25 +0200

    hw2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,5 +5,26 @@ const player = new Player(iframe);
 
 player.on('timeupdate', function (time) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
-  // console.log('played the video!');
 });
+
+const saveTime = localStorage.getItem('videoplayer-current-time');
+// console.log(saveTime);
+const timeStop = JSON.parse(saveTime);
+// console.log(timeStop.seconds);
+
+player
+  .setCurrentTime(timeStop.seconds)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
