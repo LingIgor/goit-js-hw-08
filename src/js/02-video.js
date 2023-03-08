501e41f0d42a262415fa4ce46e83850107450d0a
Ling Igor <lingigor1990@gmail.com>
2023-03-08 18:04:55 +0200

    hw8 - 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,17 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-player.on('timeupdate', function (time) {
+const timeUpdate = function (time) {
   localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
-});
+};
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
 
 const saveTime = localStorage.getItem('videoplayer-current-time');
 // console.log(saveTime);
 const timeStop = JSON.parse(saveTime);
 // console.log(timeStop.seconds);
 
-player
-  .setCurrentTime(timeStop.seconds)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(timeStop.seconds || 0);
