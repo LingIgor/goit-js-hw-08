565a21ed2dadbc98ebc096210262f2b990fb962c
Ling Igor <lingigor1990@gmail.com>
2023-03-12 15:20:45 +0200

    hw 2 fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,22 @@
 import throttle from 'lodash.throttle';
+
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
+
 const player = new Player(iframe);
+
 const timeUpdate = function (time) {
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(time.seconds)
+  );
 };
 
 player.on('timeupdate', throttle(timeUpdate, 1000));
 
 const saveTime = localStorage.getItem('videoplayer-current-time');
-// console.log(saveTime);
-const timeStop = JSON.parse(saveTime);
-// console.log(timeStop.seconds);
 
-player.setCurrentTime(timeStop.seconds || 0);
+const timeStop = JSON.parse(saveTime);
+
+player.setCurrentTime(timeStop);
